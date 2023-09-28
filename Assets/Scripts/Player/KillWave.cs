f003205fb5353be724cc722df50d0bea0d8e6b58
NichollasFarias <ni.chollas@outlook.com>
2023-09-28 16:53:38 -0300

    Segunda Tentativa

---
+++ Assets/Scripts/Player/KillWave.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KillWave : MonoBehaviour
+{
+    public GameObject Shoot;
+    public Transform local;
+    public MvtPlayer Player;
+    public float velocity = 6000f;
+    public float time = 0;
+
+    void Update()
+    {
+        if (time > 0)
+        {
+            time -= Time.deltaTime;
+        }
+
+        if (time <= 0 && Input.GetButtonDown("Wave"))
+        {
+            GetComponent<Animator>().SetTrigger("Wave");
+            Player.enabled = false;
+        }
+
+    }
+
+    public void Wave()
+    {
+        GameObject tirodisparado = (GameObject)Instantiate(Shoot, local.position, Quaternion.identity);
+        time = 2f;
+    }
+
+    public void CanMove()
+    {
+        Player.enabled = true;
+    }
+}
